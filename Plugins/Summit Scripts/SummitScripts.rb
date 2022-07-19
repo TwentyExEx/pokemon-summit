ed359720fee82cf344d804031cb37c48b0612797
kaistella <59009017+kaistella@users.noreply.github.com>
2022-07-19 19:17:13 +0800

    move tutor

--- Plugins/Summit Scripts/SummitScripts.rb
+++ Plugins/Summit Scripts/SummitScripts.rb
@@ -1505,3 +1505,66 @@ def pbSummitMonkey
     pbSummitGivePokemon(:SIMISEAR,0)
     pbSummitGivePokemon(:SIMIPOUR,0)
 end
+
+def pbDrawMoveList 
+  overlay=@sprites["overlay"].bitmap
+  overlay.clear
+  type1_number = GameData::Type.get(@pokemon.type1).id_number
+  type2_number = GameData::Type.get(@pokemon.type2).id_number
+  type1rect=Rect.new(0, type1_number * 28, 64, 28)
+  type2rect=Rect.new(0, type2_number * 28, 64, 28)
+  if @pokemon.type1==@pokemon.type2
+    overlay.blt(400,70,@typebitmap.bitmap,type1rect)
+  else
+    overlay.blt(366,70,@typebitmap.bitmap,type1rect)
+    overlay.blt(436,70,@typebitmap.bitmap,type2rect)
+  end
+  textpos=[
+     [_INTL("Teach which move?"),16,2,0,Color.new(88,88,80),Color.new(168,184,184)]
+  ]
+  imagepos=[]
+  yPos=76
+  for i in 0...VISIBLEMOVES
+    moveobject=@moves[@sprites["commands"].top_item+i]
+    if moveobject
+      moveData=GameData::Move.get(moveobject)
+      type_number = GameData::Type.get(moveData.type).id_number
+      imagepos.push(["Graphics/Pictures/types", 12, yPos + 8, 0, type_number * 28, 64, 28])
+      textpos.push([moveData.name,80,yPos,0,Color.new(248,248,248),Color.new(0,0,0)])
+      if learnedmoves.include?(i)
+        textpos.push([_INTL("Cost:"),112,yPos+32,0,Color.new(64,64,64),Color.new(176,176,176)])
+        textpos.push([_INTL("Free",moveData.total_pp),230,yPos+32,1,
+           Color.new(64,64,64),Color.new(176,176,176)])
+      else
+        textpos.push(["Cost:",80,yPos,0,Color.new(64,64,64),Color.new(176,176,176)])
+        textpos.push(["$500",228,yPos+32,1,Color.new(64,64,64),Color.new(176,176,176)])
+      end
+    end
+    yPos+=64
+  end
+  imagepos.push(["Graphics/Pictures/reminderSel",
+     0,78+(@sprites["commands"].index-@sprites["commands"].top_item)*64,
+     0,0,258,72])
+  selMoveData=GameData::Move.get(@moves[@sprites["commands"].index])
+  basedamage=selMoveData.base_damage
+  category=selMoveData.category
+  accuracy=selMoveData.accuracy
+  textpos.push([_INTL("CATEGORY"),272,108,0,Color.new(248,248,248),Color.new(0,0,0)])
+  textpos.push([_INTL("POWER"),272,140,0,Color.new(248,248,248),Color.new(0,0,0)])
+  textpos.push([basedamage<=1 ? basedamage==1 ? "???" : "---" : sprintf("%d",basedamage),
+        468,140,2,Color.new(64,64,64),Color.new(176,176,176)])
+  textpos.push([_INTL("ACCURACY"),272,172,0,Color.new(248,248,248),Color.new(0,0,0)])
+  textpos.push([accuracy==0 ? "---" : "#{accuracy}%",
+        468,172,2,Color.new(64,64,64),Color.new(176,176,176)])
+  pbDrawTextPositions(overlay,textpos)
+  imagepos.push(["Graphics/Pictures/category",436,116,0,category*28,64,28])
+  if @sprites["commands"].index<@moves.length-1
+    imagepos.push(["Graphics/Pictures/reminderButtons",48,350,0,0,76,32])
+  end
+  if @sprites["commands"].index>0
+    imagepos.push(["Graphics/Pictures/reminderButtons",134,350,76,0,76,32])
+  end
+  pbDrawImagePositions(overlay,imagepos)
+  drawTextEx(overlay,272,214,230,5,selMoveData.description,
+     Color.new(64,64,64),Color.new(176,176,176))
+end
