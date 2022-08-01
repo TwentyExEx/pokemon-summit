5e5850bdca3ec7d0485883eb0b6b69ba6f498524
James <goffoTV@gmail.com>
2022-08-01 16:20:33 +1000

    Bosses + some edit of announcer

--- PBS/BrokenDown/TypeBasedAnnouncer.rb
+++ PBS/BrokenDown/TypeBasedAnnouncer.rb
@@ -1,41 +1,41 @@
    normalmessages = [
     ["Their Pokémon may be Normal, but #{opp} is nothing but extraordinary!","Challenger #{$player.name} really has a tough fight ahead!"],
-	["Don't turn off your TV sets folks, Normal types can surprise you!","You can count on #{opp} and Challenger #{$player.name} putting on a show! "],
-	["#{opp}, master of Normal type Pokémon, is here to fight!","They are tough, but Challenger #{$player.name} has a Fighting chance!"]
+	["Don't turn off your TV sets folks, Normal-Types can surprise you!","You can count on #{opp} and Challenger #{$player.name} putting on a show! "],
+	["#{opp}, master of Normal-Type Pokémon, is here to fight!","They are tough, but Challenger #{$player.name} has a Fighting chance!"]
   ]
  
    fightingmessages = [
     ["Straight from the dojo, #{opp} is here to show us the ropes!","Challenger #{$player.name} better bring their best!"],
     ["With Pokémon that can chop right through concrete, #{opp} enters the ring!","Will this force prove too strong for Challenger #{$player.name}?"],
-    ["#{opp} has a whole team of intense Fighting type Pokémon","Challenger #{$player.name} would need to be a Psychic to break down this trainer!"]
+    ["#{opp} has a whole team of intense Fighting-Type Pokémon","Challenger #{$player.name} would need to be a Psychic to break down this trainer!"]
   ]
   
    flyingmessages = [
     ["Watch out above Challenger #{$player.name}!","#{opp} swoops into the arena!"],
     ["#{opp} takes to the skies to fight Challenger #{$player.name} with their Flying Pokémon!","The cameramen will need a break after this one!"],
-    ["With all of their Flying type Pokémon, it'll be hard to catch #{opp} off guard!","Will Challenger #{$player.name} find a way to Rock their foundations?"]
+    ["With all of their Flying-Type Pokémon, it'll be hard to catch #{opp} off guard!","Will Challenger #{$player.name} find a way to Rock their foundations?"]
   ]
   
    bugmessages = [
     ["Oh my... so many Bug Pokémon...","I'm sorry Challenger #{$player.name} and #{opp}, I just can't watch this one"],
     ["#{opp} has a whole team of creepy-crawly Bug Pokémon!","I don't know how Challenger #{$player.name} can be in the same room..."],
-    ["I try not to be bias, but the Bug type Pokémon of #{opp} give me no choice","Send 'em Flying, Challenger #{$player.name}"]
+    ["I try not to be bias, but the Bug-Type Pokémon of #{opp} give me no choice.","Send 'em Flying, Challenger #{$player.name}"]
   ]
   
    ghostmessages = [
     ["Quiet whispers fill the room as the haunting battle between Challenger #{$player.name} and #{opp} begins!"],
     ["Challenger #{$player.name} has entered the arena, looking confident and ready to fight!","However, #{opp} has a whole lot of spirit!"],
-    ["#{opp} is here to terrify with their horde of Ghost type Pokémon!","Times ahead certainly look Dark for Challenger #{$player.name}!"],
+    ["#{opp} is here to terrify with their horde of Ghost-Type Pokémon!","Times ahead certainly look Dark for Challenger #{$player.name}!"],
   ]
   
    steelmessages = [
-    ["Shining of brilliant silver, #{opp} and their Steel type Pokémon are here to put pressure on Challenger #{$player.name} and their team!"],
-    ["Challenger #{$player.name} vs #{opp} live on your screens!","This battle will be metal!"],
-    ["With their sturdy Steel type Pokémon, #{opp} has come to shake things up!","Challenger #{$player.name} will have no choice but to hit the Ground running!"],
+    ["Shining of brilliant silver, #{opp} and their Steel-Type Pokémon are here to put pressure on Challenger #{$player.name} and their team!"],
+    ["Challenger #{$player.name} vs #{opp} live on your screens in a moment.","This battle will be metal!"],
+    ["With their sturdy Steel-Type Pokémon, #{opp} has come to shake things up.","Challenger #{$player.name} will have no choice but to hit the Ground running!"],
   ]
   
    firemessages = [
     ["With a flaming fury, #{opp} bursts onto our screens!","Can Challenger #{$player.name} find a way to control the flames?"],
-    ["Challenger #{$player.name} vs #{opp} will surely make sparks fly!","Things are really heating up around here!"],
-    ["#{opp} makes training Fire type Pokémon look easy!","Challenger #{$player.name} is in troubled Water in this matchup!"],
+    ["Challenger #{$player.name} vs #{opp} will surely make sparks fly.","Things are really heating up around here!"],
+    ["#{opp} makes training Fire-Type Pokémon look easy.","Challenger #{$player.name} is in troubled Water in this matchup!"],
   ]
