23ecc18d0a421b2df93260c15fdb2fea66b9cd85
TwentyExEx <goffoTV@gmail.com>
2023-01-26 22:31:51 +1000

    tEST

--- Plugins/Summit Scripts/SummitPokeTest.rb
+++ Plugins/Summit Scripts/SummitPokeTest.rb
@@ -1,69 +1,39 @@
 module SummitPokeTest
 	# Array of all species listed
-	@allspecies = [:PKMN1] # ,:PKMN2,:PKMN3,:PKMN4,:PKMN5,:PKMN6]
+	@allspecies = [:PKMN1,:PKMN2,:PKMN3]
 
 	def self.allspecies
 		return @allspecies
 	end
 
 	PKMN1 = {
-		:species => :WOBBUFFET,
-		:tera_type => :NORMAL,
-		:moves => [:ENCORE,:COUNTER,:MIRRORCOAT,:DESTINYBOND],
+		:species => :CLEFABLE,
+		:tera_type => :FAIRY,
+		:moves => [:STEALTHROCK,:MOONBLAST,:SOFTBOILED,:KNOCKOFF],
 		:form => 0,
 		:ability_index => 1,
-		# :item => :LIFEORB,
+		:item => :LEFTOVERS,
+		:nature => :CALM,
+		:evs => [252,0,200,0,0,56]
+	}
+	PKMN2 = {
+		:species => :LOPUNNY,
+		:tera_type => :FIGHTING,
+		:moves => [:CLOSECOMBAT,:UTURN,:LASHOUT,:RETURN],
+		:form => 0,
+		:ability_index => 2,
+		:item => :CHOICEBAND,
 		:nature => :JOLLY,
 		:evs => [0,252,0,252,0,4]
 	}
-	# PKMN2 = {
-	# 	:species => :AERODACTYL,
-	# 	:tera_type => :ELECTRIC,
-	# 	:moves => [:DRAGONDANCE,:DUALWINGBEAT,:THUNDERFANG,:ROCKSLIDE],
-	# 	:form => 0,
-	# 	:ability_index => 1,
-	# 	:item => :FOCUSSASH,
-	# 	:nature => :ADAMANT,
-	# 	:evs => [0,252,0,252,0,4]
-	# }
-	# PKMN3 = {
-	# 	:species => :GOLEM,
-	# 	:tera_type => :ELECTRIC,
-	# 	:moves => [:STONEEDGE,:BODYPRESS,:EARTHQUAKE,:FIREPUNCH],
-	# 	:form => 0,
-	# 	:ability_index => 1,
-	# 	:item => :FOCUSSASH,
-	# 	:nature => :ADAMANT,
-	# 	:evs => [252,252,0,0,0,4]
-	# }
-	# PKMN4 = {
-	# 	:species => :GOLEM,
-	# 	:tera_type => :ELECTRIC,
-	# 	:moves => [:STONEEDGE,:BODYPRESS,:EARTHQUAKE,:FIREPUNCH],
-	# 	:form => 0,
-	# 	:ability_index => 1,
-	# 	:item => :FOCUSSASH,
-	# 	:nature => :ADAMANT,
-	# 	:evs => [252,252,0,0,0,4]
-	# }
-	# PKMN5 = {
-	# 	:species => :GOLEM,
-	# 	:tera_type => :ELECTRIC,
-	# 	:moves => [:STONEEDGE,:BODYPRESS,:EARTHQUAKE,:FIREPUNCH],
-	# 	:form => 0,
-	# 	:ability_index => 1,
-	# 	:item => :FOCUSSASH,
-	# 	:nature => :ADAMANT,
-	# 	:evs => [252,252,0,0,0,4]
-	# }
-	# PKMN6 = {
-	# 	:species => :GOLEM,
-	# 	:tera_type => :ELECTRIC,
-	# 	:moves => [:STONEEDGE,:BODYPRESS,:EARTHQUAKE,:FIREPUNCH],
-	# 	:form => 0,
-	# 	:ability_index => 1,
-	# 	:item => :FOCUSSASH,
-	# 	:nature => :ADAMANT,
-	# 	:evs => [252,252,0,0,0,4]
-	# }
+	PKMN3 = {
+		:species => :WOBBUFFET,
+		:tera_type => :GHOST,
+		:moves => [:ENCORE,:COUNTER,:MIRRORCOAT,:DESTINYBOND],
+		:form => 0,
+		:ability_index => 1,
+		:item => :AGUAVBERRY,
+		:nature => :CALM,
+		:evs => [252,0,4,0,0,252]
+	}
 end
