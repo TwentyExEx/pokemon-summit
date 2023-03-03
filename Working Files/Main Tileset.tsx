8b854ee1afc7a3bcf01ab439601ec2dc66196125
James Magoffin <goffoTV@gmail.com>
2023-03-03 14:22:27 +1000

    Test map stuff

---
+++ Working Files/Main Tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Main Tileset" tilewidth="32" tileheight="32" tilecount="6032" columns="8">
+ <image source="../Graphics/Tilesets/Outside_custom.png" width="256" height="24146"/>
+</tileset>
